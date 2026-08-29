53174851f272a9a822760dbce15e5344669f16e9
Aswin <117593557+AswinM1@users.noreply.github.com>
2026-08-29 11:55:55 +0530

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { RootProvider } from "fumadocs-ui/provider/next";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -60,6 +61,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           }}>
           {children}
         </RootProvider>
+         <Analytics />
         </body>
     </html>
   );
